6f56ba08fca356dd89ad570a42f217e0e24432b0
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-11-04 10:00:41 +0000

    Working for content!

--- Our.Umbraco.CropHealer/CropHealer.cs
+++ Our.Umbraco.CropHealer/CropHealer.cs
@@ -6,7 +6,231 @@ using System.Threading.Tasks;
 
 namespace Our.Umbraco.CropHealer
 {
+    using System.Globalization;
+
+    using global::Umbraco.Core;
+    using global::Umbraco.Core.Logging;
+    using global::Umbraco.Core.Models;
+    using global::Umbraco.Core.Services;
+    using global::Umbraco.Web.Models;
+
+    using Newtonsoft.Json;
+
     public static class CropHealer
     {
+        internal static void SeekandHeal(IDataTypeDefinition dataType, IDataTypeService dts)
+        {
+            var cts = ApplicationContext.Current.Services.ContentTypeService;
+            var cs = ApplicationContext.Current.Services.ContentService;
+            var mts = ApplicationContext.Current.Services.MemberTypeService;
+
+            var dataTypeId = dataType.Id;
+            var propertyEditor = dataType.PropertyEditorAlias;
+            if (propertyEditor == Constants.PropertyEditors.ImageCropperAlias)
+            {
+                // A Image Cropper Data Type has been saved, lets go to work!
+                var allContentTypes = cts.GetAllContentTypes();
+                var allMediaTypes = cts.GetAllMediaTypes();
+                var allMemberTypes = mts.GetAll();
+
+                foreach (var contentType in allContentTypes)
+                {
+                    var contentTypeId = contentType.Id;
+                    var cropperContentProperties = contentType.PropertyTypes.Where(x => x.DataTypeDefinitionId == dataTypeId);
+                    if (cropperContentProperties.Any())
+                    {
+                        var allContentUsingThis = cs.GetContentOfContentType(contentTypeId); 
+                        HealContentItems(allContentUsingThis, cropperContentProperties, dataTypeId, dts, cs);
+                    }
+                }
+
+                // ** DON'T FORGET THE MEDIA and MEMBERS **
+
+            }
+        }
+
+        private static void HealContentItems(IEnumerable<IContent> content, IEnumerable<PropertyType> cropperContentProperties, int dataTypeId, IDataTypeService dts, IContentService cs)
+        {
+            foreach (var contentItem in content)
+            {
+                // There could be multiple uses of the same datatype on the content type 
+                foreach (var cropperContentProperty in cropperContentProperties)
+                {
+                    var cropperPropertyValue = contentItem.GetValue<string>(cropperContentProperty.Alias);
+
+                    var cropDataSet = cropperPropertyValue.SerializeToCropDataSet();
+
+                    var attemptHeal = GetCropFromDataType(dataTypeId, cropDataSet, cropperPropertyValue, dts);
+                    if (attemptHeal != null)
+                    {
+                        contentItem.SetValue(cropperContentProperty.Alias, attemptHeal);
+                    }
+                }
+
+                if (contentItem.Published)
+                {
+                    cs.SaveAndPublishWithStatus(contentItem);
+                    LogHelper.Info(
+                        typeof(CropHealer),
+                        string.Format(
+                            "Healed a Image Cropper in content (NodeId:{0} DocumentTypeAlias:{1}) and Published",
+                            contentItem.Id,
+                            contentItem.ContentType.Alias));
+                }
+                else
+                {
+                    cs.Save(contentItem);
+                    LogHelper.Info(
+                        typeof(CropHealer),
+                        string.Format(
+                            "Healed a Image Cropper in content (NodeId:{0} DocumentTypeAlias:{1}) but didn't publish as it contained unpublished content",
+                            contentItem.Id,
+                            contentItem.ContentType.Alias));
+                }
+            }
+        }
+
+        private static string GetCropFromDataType(int dataTypeId, ImageCropDataSet cropDataSet, string json, IDataTypeService dts)
+        {
+            var preValueJson =
+                dts.GetPreValuesCollectionByDataTypeId(dataTypeId)
+                    .PreValuesAsDictionary.FirstOrDefault(x => x.Key.ToLower(CultureInfo.InvariantCulture) == "crops")
+                    .Value.Value;
+
+            var cropperPreValues = JsonConvert.DeserializeObject<List<ImageCropData>>(preValueJson);
+
+            if (cropperPreValues != null && cropperPreValues.Any())
+            {
+                var attemptHeal = ImageCropDataSetRepair(cropDataSet, json, cropperPreValues);
+                return attemptHeal;
+            }     
+
+            return null;
+        }
+
+        private static string ImageCropDataSetRepair(this ImageCropDataSet cropDataSet, string json, List<ImageCropData> imageCrops = null)
+        {
+            var healedSomething = false;
+
+            if (cropDataSet.Src == null)
+            {
+                // convert from image path to cropper
+                cropDataSet.Src = json;
+                healedSomething = true;
+            }
+
+            if (cropDataSet.FocalPoint == null)
+            {
+                cropDataSet.FocalPoint = new ImageCropFocalPoint() { Left = 0.5m, Top = 0.5m };
+                healedSomething = true;
+            }
+
+            if (imageCrops != null && imageCrops.Any())
+            {
+
+                var currentCrops = new List<ImageCropData>();
+
+                if (cropDataSet.Crops != null)
+                {
+                    currentCrops = cropDataSet.Crops.ToList();
+                    // delete crops that have been removed from the data type
+                    foreach (var crop in cropDataSet.Crops)
+                    {
+                        if (!imageCrops.Select(x => x.Alias).Contains(crop.Alias))
+                        {
+                            currentCrops.Remove(crop);
+                            healedSomething = true;
+                        }
+                    }
+
+                }
+
+                foreach (var cropDef in imageCrops)
+                {
+                    var cropDefInCurrent = currentCrops.FirstOrDefault(x => x.Alias == cropDef.Alias);
+
+                    // check if width or height different
+                    if (cropDefInCurrent != null && (cropDef.Width != cropDefInCurrent.Width || cropDef.Height != cropDefInCurrent.Height))
+                    {
+                        var index = currentCrops.IndexOf(cropDefInCurrent);
+
+                        cropDefInCurrent.Width = cropDef.Width;
+                        cropDefInCurrent.Height = cropDef.Height;
+
+                        currentCrops.RemoveAt(index);
+                        currentCrops.Insert(index, cropDefInCurrent);
+                        healedSomething = true;
+                    }
+
+                    if (!currentCrops.Select(x => x.Alias).Contains(cropDef.Alias))
+                    {
+                        currentCrops.Add(cropDef);
+                        healedSomething = true;
+                    }
+
+                }
+
+                if (cropDataSet.Crops == null)
+                {
+                    cropDataSet.Crops = new List<ImageCropData>();
+                    healedSomething = true;
+                }
+
+                cropDataSet.Crops = currentCrops;
+            }
+
+            if (healedSomething)
+            {
+                return JsonConvert.SerializeObject(
+                    cropDataSet,
+                    new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            }
+
+            return null;
+        }
+
+        private static bool DetectIsJson(this string input)
+        {
+            input = input.Trim();
+            return (input.StartsWith("{") && input.EndsWith("}"))
+                   || (input.StartsWith("[") && input.EndsWith("]"));
+        }
+
+        private static ImageCropDataSet SerializeToCropDataSet(this string json)
+        {
+            var imageCrops = new ImageCropDataSet();
+            if (json.DetectIsJson())
+            {
+                try
+                {
+                    imageCrops = JsonConvert.DeserializeObject<ImageCropDataSet>(json);
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error(typeof(CropHealer), "Could not parse the json string: " + json, ex);
+                }
+            }
+
+            return imageCrops;
+        }
+
+        private static ImageCropData GetCrop(this ImageCropDataSet dataset, string cropAlias)
+        {
+            if (dataset == null || dataset.Crops == null || !dataset.Crops.Any())
+                return null;
+
+            return dataset.Crops.GetCrop(cropAlias);
+        }
+
+        private static ImageCropData GetCrop(this IEnumerable<ImageCropData> dataset, string cropAlias)
+        {
+            if (dataset == null || !dataset.Any())
+                return null;
+
+            if (string.IsNullOrEmpty(cropAlias))
+                return dataset.FirstOrDefault();
+
+            return dataset.FirstOrDefault(x => x.Alias.ToLowerInvariant() == cropAlias.ToLowerInvariant());
+        }
     }
 }
