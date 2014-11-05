2effd12a8d148facc6e345eaffcd594a8b8ce3f9
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-11-05 11:15:53 +0000

    Adding BuildTools

--- Our.Umbraco.CropHealer/Properties/AssemblyInfo.cs
+++ Our.Umbraco.CropHealer/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Our.Umbraco.CropHealer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ?  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-beta")]
+// [assembly: AssemblyVersion("1.0.0.100")]
+[assembly: AssemblyVersion("1.0.0.100")]
+[assembly: AssemblyFileVersion("1.0.0.100")]
+[assembly: AssemblyInformationalVersion("1.0.0-build100")]
