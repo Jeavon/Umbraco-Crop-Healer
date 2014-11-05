49b6e9f4cb7bce571fdbdf7544ddfb0a8997bd89
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-11-05 15:50:10 +0000

    Ensure build.bat is building tests project!

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -4,3 +4,4 @@ ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 cd ..\BuildPackage\
 Call Tools\nuget.exe restore ..\Our.Umbraco.CropHealer.sln
 Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" Package.build.xml
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" ..\Our.Umbraco.CropHealer.Tests\Our.Umbraco.CropHealer.Tests.csproj
