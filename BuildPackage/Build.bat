f8441bd301ea4bf9e16ac88a58f378118cfc00ca
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-02-19 11:40:59 +0000

    Update build scripts for GitHub tags support

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -3,5 +3,5 @@ ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 cd ..\BuildPackage\
 Call Tools\nuget.exe restore ..\Our.Umbraco.CropHealer.sln
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" Package.build.xml
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
 Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" ..\Our.Umbraco.CropHealer.Tests\Our.Umbraco.CropHealer.Tests.csproj
