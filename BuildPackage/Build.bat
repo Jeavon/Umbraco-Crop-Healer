2fb5e1df666bf17e2359104c02ade4580e153bd6
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-02-19 11:59:55 +0000

    Switching test compile to release

--- BuildPackage/Build.bat
+++ BuildPackage/Build.bat
@@ -4,4 +4,4 @@ ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 cd ..\BuildPackage\
 Call Tools\nuget.exe restore ..\Our.Umbraco.CropHealer.sln
 Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" ..\Our.Umbraco.CropHealer.Tests\Our.Umbraco.CropHealer.Tests.csproj
+Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" ..\Our.Umbraco.CropHealer.Tests\Our.Umbraco.CropHealer.Tests.csproj /p:Configuration=Release
