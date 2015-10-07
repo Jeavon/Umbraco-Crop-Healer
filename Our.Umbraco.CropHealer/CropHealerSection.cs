2a2fd4d6ff76753506086dd8cee17b6c1f6aad43
Jeavon <jeavon@crumpled-dog.com>
2015-10-07 17:49:29 +0100

    Started work on configuration for exclusions

---
+++ Our.Umbraco.CropHealer/CropHealerSection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Our.Umbraco.CropHealer
+{
+    public class CropHealerSection : ConfigurationSection
+    {
+        public ExclusionsElement Exclusions
+        {
+            get { return (ExclusionsElement) this["exclusions"]; }
+            set { this["exclusions"] = value; }
+        }
+    }
+
+    public class ExclusionsElement : ConfigurationElement
+    {        
+    }
+}
