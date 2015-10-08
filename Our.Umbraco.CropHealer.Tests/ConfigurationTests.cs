875b058e03515386d3d31402683599e3873f8c21
Jeavon <jeavon@crumpled-dog.com>
2015-10-08 12:39:08 +0100

    Add CropHealer.config models

---
+++ Our.Umbraco.CropHealer.Tests/ConfigurationTests.cs
@@ -0,0 +1,29 @@
+﻿namespace Our.Umbraco.CropHealer.Tests
+{
+    using System.Configuration;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void BasicDocumentTypeElementTest()
+        {
+            var section = ConfigurationManager.GetSection("CropHealer")
+                             as CropHealerConfigSection;
+
+            Assert.AreEqual(section.Exclusions.DocumentTypes.FirstOrDefault().Alias, "umbNewsItem");
+        }
+
+        [Test]
+        public void BasicMediaypeElementTest()
+        {
+            var section = ConfigurationManager.GetSection("CropHealer")
+                             as CropHealerConfigSection;
+
+            Assert.AreEqual(section.Exclusions.MediaTypes.FirstOrDefault().Alias, "Image");
+        }
+    }
+}
--- Our.Umbraco.CropHealer/CropHealerSection.cs
+++ Our.Umbraco.CropHealer/CropHealerSection.cs
@@ -7,8 +7,9 @@ using System.Threading.Tasks;
 
 namespace Our.Umbraco.CropHealer
 {
-    public class CropHealerSection : ConfigurationSection
+    public class CropHealerConfigSection : ConfigurationSection
     {
+        [ConfigurationProperty("exclusions", IsDefaultCollection = true)]
         public ExclusionsElement Exclusions
         {
             get { return (ExclusionsElement) this["exclusions"]; }
@@ -17,6 +18,52 @@ namespace Our.Umbraco.CropHealer
     }
 
     public class ExclusionsElement : ConfigurationElement
-    {        
+    {
+        [ConfigurationProperty("documentTypes", IsDefaultCollection = true)]
+        public TypeElementCollection DocumentTypes
+        {
+            get { return (TypeElementCollection) this["documentTypes"]; }
+            set { this["documentTypes"] = value; }
+        }
+
+        [ConfigurationProperty("mediaTypes", IsDefaultCollection = true)]
+        public TypeElementCollection MediaTypes
+        {
+            get { return (TypeElementCollection)this["mediaTypes"]; }
+            set { this["mediaTypes"] = value; }
+        }
+    }
+
+    public class TypeElement : ConfigurationElement
+    {
+        [ConfigurationProperty("alias", IsKey = true, IsRequired = true)]
+        public string Alias
+        {
+            get { return (string)this["alias"]; }
+            set { this["alias"] = value; }
+        }
+    }
+
+    [ConfigurationCollection(typeof(TypeElement))]
+    public class TypeElementCollection : ConfigurationElementCollection, IEnumerable<TypeElement>
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new TypeElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((TypeElement)element).Alias;
+        }
+
+        public new IEnumerator<TypeElement> GetEnumerator()
+        {
+            int count = base.Count;
+            for (int i = 0; i < count; i++)
+            {
+                yield return base.BaseGet(i) as TypeElement;
+            }
+        }
     }
 }
