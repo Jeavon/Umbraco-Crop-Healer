62be085f6ce2ed78aea4920def4ffe7a80e560d5
Jeavon <jeavon@crumpled-dog.com>
2015-10-12 19:07:17 +0100

    Adding uninstall xdt

--- Our.Umbraco.CropHealer/Properties/AssemblyInfo.cs
+++ Our.Umbraco.CropHealer/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.1.123")]
-[assembly: AssemblyVersion("1.1.1.123")]
-[assembly: AssemblyFileVersion("1.1.1.123")]
-[assembly: AssemblyInformationalVersion("1.1.1-build123")]
+// [assembly: AssemblyVersion("0.1.1.126")]
+[assembly: AssemblyVersion("0.1.1.126")]
+[assembly: AssemblyFileVersion("0.1.1.126")]
+[assembly: AssemblyInformationalVersion("0.1.1-build126")]
